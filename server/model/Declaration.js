3fd12b6c690fd1cf4994e4dc77fb782da3d82105
Clavien317 <Claviennambinina511@gmail.com>
2024-08-28 23:08:02 +0300

    doing model

--- server/model/Declaration.js
+++ server/model/Declaration.js
@@ -5,8 +5,6 @@ const schema = new mongoose.Schema({
     id_user:{type: String,required: true},
     titre:{type: String,required: true},
     detailsDonation:{type: String,required: true},
-    Actif:{type: String,required: false},
-    Passif:{type: String,required: false},
     affirmationSincerite:{type: String,required: true},
     dateSoumission:{type: String,required: false},
     etat:{type: String,required: false},
